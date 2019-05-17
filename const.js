742a6513616118b0e149bf42fc20a4e2cfd68ff8
Melvin SCHULTZ <mschultz@kaliop.com>
2019-05-17 10:36:45 +0200

    first step: generate a csv file from js file, todo: second step, generate a gs file from js file

--- const.js
+++ const.js
@@ -1,6 +1,5 @@
 module.exports = {
   MANDATORY_FIELDS: {
-    GSID: 'gsid',
     CREDENTIALS_PATH: 'credentialsPath'
   }
 }
--- generate-translate.js
+++ generate-translate.js
@@ -7,11 +7,13 @@ const util = require('util')
 const jsBeautifier = require('js-beautify')
 const yamlBeautifier = require('align-yaml')
 const CONST = require('./const')
+const createCsvWriter = require('csv-writer').createObjectCsvWriter
 
 const defaultOptions = {
   outputFormat: 'js',
   locale: 'fr',
   gid: '',
+  sourceDir: './',
   outputDir: './'
 }
 
@@ -20,13 +22,14 @@ const optionDefinitions = [
   {name: 'gsid', alias: 'g', type: String},
   {name: 'outputFormat', alias: 'o', type: String},
   {name: 'help', alias: 'h', type: Boolean},
+  {name: 'sourceDir', alias: 's', type: String},
   {name: 'outputDir', alias: 'd', type: String},
   {name: 'credentialsPath', alias: 'c', type: String}
 ]
 
 const cliOptions = commandLineArgs(optionDefinitions)
-const mandatoryFields = [CONST.MANDATORY_FIELDS.GSID, CONST.MANDATORY_FIELDS.CREDENTIALS_PATH]
-const allowedOutputFormat = ['js']
+const mandatoryFields = [CONST.MANDATORY_FIELDS.CREDENTIALS_PATH]
+const allowedOutputFormat = ['js', 'csv']
 
 if (cliOptions.hasOwnProperty('help')) {
   help()
@@ -58,47 +61,68 @@ if (!allowedOutputFormat.includes(options['outputFormat'])) {
   process.exit(1)
 }
 
-const spreadsheet = new GoogleSpreadsheet(options['gsid'])
-const authPromise = bluebird.promisify(spreadsheet.useServiceAccountAuth)
-const getRowsPromise = bluebird.promisify(spreadsheet.getRows)
+if (options['outputFormat'] === 'js') {
+  const spreadsheet = new GoogleSpreadsheet(options['gsid'])
+  const authPromise = bluebird.promisify(spreadsheet.useServiceAccountAuth)
+  const getRowsPromise = bluebird.promisify(spreadsheet.getRows)
 
-console.log('Authenticating...')
+  console.log('Authenticating...')
 
-return authPromise({
-  client_email: credentials['client_email'],
-  private_key: credentials['private_key']
-})
-.then(() => {
-  console.log('Fetching cells...')
-  return getRowsPromise(1, {'offset': 1,'limit': 10000})
-})
-.then(data => {
-  console.log('Generating translations file...')
-  let translatedPayload = {}
-  data.forEach(row => {
-    translatedPayload[row['translationkey']] = row['translatedtext']
+  return authPromise({
+    client_email: credentials['client_email'],
+    private_key: credentials['private_key']
   })
+  .then(() => {
+    console.log('Fetching cells...')
+    return getRowsPromise(1, {'offset': 1,'limit': 10000})
+  })
+  .then(data => {
+    console.log('Generating translations file...')
+    let translatedPayload = {}
+    data.forEach(row => {
+      translatedPayload[row['translationkey']] = row['translatedtext']
+    })
 
-  let finalPayload = {
-    [options['locale']]: unflatten(translatedPayload)
-  }
+    let finalPayload = {
+      [options['locale']]: unflatten(translatedPayload)
+    }
 
-  if (options['outputFormat'] === 'js') {
-    fs.writeFileSync(`${options['outputDir']}${options['locale']}.js`, jsBeautifier(`module.exports = ${util.inspect(finalPayload, false, null)}`, {indent_size: 2}) + '\n')
-  } 
-  // else {
-  //   fs.writeFile(
-  //     `${options['outputDir']}${options['locale']}.yaml`, 
-  //     yamlBeautifier(util.inspect(finalPayload, false, null).replace(/{/g, '').replace(/}/g, ''), 4)
-  //   )
-  // }
+    if (options['outputFormat'] === 'js') {
+      fs.writeFileSync(`${options['outputDir']}${options['locale']}.js`, jsBeautifier(`module.exports = ${util.inspect(finalPayload, false, null)}`, {indent_size: 2}) + '\n')
+    } 
+    // else {
+    //   fs.writeFile(
+    //     `${options['outputDir']}${options['locale']}.yaml`, 
+    //     yamlBeautifier(util.inspect(finalPayload, false, null).replace(/{/g, '').replace(/}/g, ''), 4)
+    //   )
+    // }
 
-  console.log('Done.')
+    console.log('Done.')
 
-})
-.catch(error => {
-  console.error(error)
-})
+  })
+  .catch(error => {
+    console.error(error)
+  })
+}
+
+if (options['outputFormat'] === 'csv') {
+  const sourceFile = require(`${options['sourceDir']}${options['locale']}.js`)
+  const entries = Object.entries(sourceFile)[0][1]
+  const data = []
+  const datas = loopThroughObject(entries, '', data)
+
+  const csvWriter = createCsvWriter({
+    path: `${options['locale']}.csv`,
+    header: [
+      {id: 'translationkey', title: 'translationkey'},
+      {id: 'translatedtext', title: 'translatedtext'}
+    ]
+  })
+  console.log(datas)
+  csvWriter
+    .writeRecords(datas)
+    .then(() => console.log('The CSV file was written successfully'))
+}
 
 function help () {
   console.log(`
@@ -118,4 +142,42 @@ node node_modules/.bin/generate-translate.js -g 14ESdKxdEktB4rLesYlIMMve6aapCT2Q
 // Generate a file name 'fr.js' based on spreadsheet ID 14ESdKxdEktB4rLesYlIMMve6aapCT2Q2jGB17F466W6mo
 `)
   process.exit(0)
+}
+
+function loopThroughObject(object, previousKey, data) {
+  Object.keys(object).map((key) => {
+    let strKey
+    if (key.match(/^\d+$/)) {
+      strKey = '[' + key + ']'
+    } else {
+      if (previousKey !== '') {
+        strKey = '.' + key
+      } else {
+        strKey = key
+      }
+    }
+
+    let prevKey
+    if (previousKey !== '') {
+      prevKey = previousKey + strKey
+    } else {
+      prevKey = strKey
+    }
+
+    if (typeof object[key] !== 'object') {
+      // console.log('-----------------------------------------------------------------------------------------------------------')
+      // console.log(prevKey + ' | ' + object[key])
+
+      const obj = {
+        'translationkey': prevKey,
+        'translatedtext': object[key]
+      }
+
+      data.push(obj)
+    } else {
+      loopThroughObject(object[key], prevKey, data)
+    }
+  })
+
+  return data
 }
